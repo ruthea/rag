2e59a748ca49a8bc65e7714aa9a54701cc1dfef9
Austin Rutherford <austinrutherford@gmail.com>
2024-10-24 09:40:06 -0500

    Update embedding.py

--- utils/embedding.py
+++ utils/embedding.py
@@ -1,9 +1,18 @@
 import json
 import boto3
 
-def get_embedding(text):
+def get_embedding(text, model_id="amazon.titan-embed-text-v2:0"):
+    """
+    Gets an embedding from a model (defaults to titan-embed-text-v2).
+
+    Args:
+        text (str): The text to convert to an embedding.
+        model_id (str, optional): The ID of the Titan model to use. Defaults to "amazon.titan-embed-text-v2:0".
+
+    Returns:
+        list: The embedding as a list of floats.
+    """
     bedrock_client = boto3.client("bedrock-runtime", region_name="us-east-1")
-    model_id = "amazon.titan-embed-text-v2:0"
 
     # Prepare request for Titan
     native_request = {"inputText": text}
@@ -15,5 +24,5 @@ def get_embedding(text):
     # Parse the response
     response_body = json.loads(response.get("body").read())
     embedding = response_body["embedding"]
-    
+
     return embedding
